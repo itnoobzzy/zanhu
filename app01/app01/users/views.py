800a96f2fbd82ce9001a403cf3e555829ecd18ca
zzy <zzy@zzy.com>
2020-08-20 15:10:11 +0800

    user视图增加模本上下文内容：用户信息

--- app01/app01/users/views.py
+++ app01/app01/users/views.py
@@ -15,6 +15,28 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     slug_field = "username"
     slug_url_kwarg = "username"
 
+    def get_context_data(self, **kwargs):
+        context = super(UserDetailView, self).get_context_data(**kwargs)
+        user = User.objects.get(username=self.request.user.username)
+        context["moments_num"] = user.publisher.filter(reply=False).count()  # 'publisher'为News模块中的related_name
+        context["article_num"] = user.author.filter(status='P').count()
+        context["comment_num"] = user.publisher.filter(reply=True).count() + user.comment_comments.all().count()
+        context["question_num"] = user.q_author.all().count()
+        context["answer_num"] = user.a_author.all().count()
+        # 互动数 = 动态点赞数 + 问答点赞数 + 评论数 + 私信用户数(都有发送或接收到私信)
+        tmp = set()
+        # 我发送私信给了多少不同的用户
+        sent_num = user.sent_message.all()
+        for i in sent_num:
+            tmp.add(i.recipient.usernmae)
+        # 我接受的所有私信来自多少不同的用户
+        received_num = user.received_messages.all()
+        for r in received_num:
+            tmp.add(r.sender.username)
+        context["interaction_num"] = user.liked_news.all().count() + user.qa_vote.all().count() + context[
+            "comment_num"] + len(tmp)
+        return context
+
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     """用户只能更新自己的信息"""
@@ -35,7 +57,6 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
             self.request, messages.INFO, _("Infos successfully updated")
         )
         return super().form_valid(form)
-
 
 # class UserRedirectView(LoginRequiredMixin, RedirectView):
 #
